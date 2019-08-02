1ca4af0aed3542a1fa9a55cc0c7ebeebf59de4fd
sparsick <sparsick@web.de>
2019-08-02 11:58:00 +0200

    add cloud demo information to see where the data come from

--- src/main/java/com/github/sparsick/springbootexample/hero/universum/HeroController.java
+++ src/main/java/com/github/sparsick/springbootexample/hero/universum/HeroController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 @Controller
 public class HeroController {
 
@@ -16,6 +19,7 @@ public class HeroController {
     @GetMapping("/hero")
     public String viewAllHeros(Model model) {
         model.addAttribute("heros", heroRepository.allHeros());
+        model.addAttribute("ipAddress", inspectLocalHost());
 
         return "hero/hero.list.html";
     }
@@ -30,6 +34,14 @@ public class HeroController {
     public String addNewHero(@ModelAttribute("hero") Hero hero){
         heroRepository.addHero(hero);
         return "redirect:/hero";
+    }
+
+    private String inspectLocalHost() {
+        try {
+            return Inet4Address.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
