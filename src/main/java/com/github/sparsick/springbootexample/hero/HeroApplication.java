7d89223f150b84dba68c38653e6263e5e53d2458
sparsick <sparsick@web.de>
2020-03-06 17:41:57 +0100

    clean up no more needed feature

--- src/main/java/com/github/sparsick/springbootexample/hero/HeroApplication.java
+++ src/main/java/com/github/sparsick/springbootexample/hero/HeroApplication.java
@@ -1,13 +1,7 @@
 package com.github.sparsick.springbootexample.hero;
 
-import com.github.sparsick.springbootexample.hero.universum.HeroRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 @SpringBootApplication
 public class HeroApplication {
--- src/main/java/com/github/sparsick/springbootexample/hero/universum/HeroRepository.java
+++ src/main/java/com/github/sparsick/springbootexample/hero/universum/HeroRepository.java
@@ -1,14 +1,35 @@
 package com.github.sparsick.springbootexample.hero.universum;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-public interface HeroRepository {
+@Repository
+public class HeroRepository  {
 
-    String getName();
+    private Set<Hero> heroes = new HashSet<>();
 
-    void addHero(Hero hero);
+    @PostConstruct
+    public void init() {
+        heroes.add(new Hero("Batman", "Gotham City", ComicUniversum.DC_COMICS));
+        heroes.add(new Hero("Superman", "Metropolis", ComicUniversum.DC_COMICS));
+    }
 
-    Collection<Hero> allHeros();
+    public void addHero(Hero hero) {
+        heroes.add(hero);
+    }
 
-    Collection<Hero> findHerosBySearchCriteria(String searchCriteria);
+    public Collection<Hero> allHeros() {
+        return new HashSet<>(heroes);
+    }
+
+    public Collection<Hero> findHerosBySearchCriteria(String searchCriteria) {
+        return heroes.stream().filter( hero -> StringUtils.containsIgnoreCase(hero.toString(), searchCriteria)).collect(Collectors.toSet());
+    }
+
 }
