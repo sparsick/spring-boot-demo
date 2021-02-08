891260e4d965ffc139a1b1c75f2bebb3ee7e6759
sparsick <sparsick@web.de>
2021-02-08 13:47:20 +0100

    update spring version 2.4.2

--- src/test/java/com/github/sparsick/springbootexample/hero/HeroApplicationTests.java
+++ src/test/java/com/github/sparsick/springbootexample/hero/HeroApplicationTests.java
@@ -1,16 +1,14 @@
 package com.github.sparsick.springbootexample.hero;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 public class HeroApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
