a32e9297dad41d18410cd201ecef9eec7d35e18b
Sandra Parsick <mail@sandra-parsick.de>
2022-06-22 12:28:19 +0200

    refactoring -  extract interface for hero repository

---
+++ src/main/java/com/github/sparsick/springbootexample/hero/universum/EmbbededHeroRepository.java
@@ -0,0 +1,38 @@
+package com.github.sparsick.springbootexample.hero.universum;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Repository
+public class EmbbededHeroRepository implements HeroRepository {
+
+    private Set<Hero> heroes = new HashSet<>();
+
+    @PostConstruct
+    public void init() {
+        heroes.add(new Hero("Batman", "Gotham City", ComicUniversum.DC_COMICS));
+        heroes.add(new Hero("Superman", "Metropolis", ComicUniversum.DC_COMICS));
+    }
+
+    @Override
+    public void addHero(Hero hero) {
+        heroes.add(hero);
+    }
+
+    @Override
+    public Collection<Hero> allHeros() {
+        return new HashSet<>(heroes);
+    }
+
+    @Override
+    public Collection<Hero> findHerosBySearchCriteria(String searchCriteria) {
+        return heroes.stream().filter( hero -> StringUtils.containsIgnoreCase(hero.toString(), searchCriteria)).collect(Collectors.toSet());
+    }
+
+}
